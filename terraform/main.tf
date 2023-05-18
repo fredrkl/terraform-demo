76eb133ba18b43f1646affba47ba4f711c926865
Fredrik Klingenberg <fredrkl@gmail.com>
2023-05-18 16:59:29 +0200

    closes: #2
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
